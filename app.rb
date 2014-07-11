c6ebeb20e0286bc461131862b600c519d1aa46e6
Tim Petricola <tim.petricola@gmail.com>
2014-07-11 13:32:20 -0400

    Put data in yml file

--- app.rb
+++ app.rb
@@ -26,50 +26,7 @@ get '/application.css' do
 end
 
 get '/' do
-  @works = {}
-
-  @works['jardins'] = {
-    name: 'Jardins au féminin',
-    images: [
-      ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
-      ['5.jpg', '6.jpg', '7.jpg', '8.jpg'],
-      ['9.jpg', '10.jpg', '11.jpg'],
-      ['12.jpg', '13.jpg', '14.jpg']
-    ]
-  }
-
-  @works['autoportraits'] = {
-    name: "Autoportraits",
-    images: [
-      ['1.jpg', '2.jpg', '3.jpg']
-    ]
-  }
-
-  @works['eau'] = {
-    name: "Le bord de l'eau",
-    images: [
-      ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
-      ['5.jpg', '6.jpg', '7.jpg']
-    ]
-  }
-
-  @works['dialogues'] = {
-    name: "Dialogues",
-    images: [
-      ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
-      ['5.jpg', '6.jpg', '7.jpg', '8.jpg'],
-      ['9.jpg']
-    ]
-  }
-
-  @works['geographies'] = {
-    name: "Géo-graphies",
-    images: [
-      ['1.jpg', '3.jpg', '2.jpg', '4.jpg'],
-      ['5.jpg', '7.jpg', '6.jpg'],
-      ['8.jpg', '9.jpg', '10.jpg']
-    ]
-  }
+  @series = YAML.load_file('./artworks.yml')['series']
 
   haml :index
 end
