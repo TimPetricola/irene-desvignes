0cd0839940da9039b35c74aaab6164529bce6436
Tim Petricola <tim.petricola@gmail.com>
2014-07-11 13:16:32 -0400

    Update ruby and gems

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'http://rubygems.org'
-gem 'sinatra'
-gem 'haml'
-gem 'sass'
-gem 'coffee-script'
-gem 'sinatra-r18n'
+ruby '2.1.2'
+
+gem 'sinatra', '~> 1.4.5'
+gem 'haml', '~> 4.0.5'
+gem 'sass', '~> 3.3.9'
+gem 'coffee-script', '~> 2.3.0'
+gem 'sinatra-r18n', '~> 1.1.11'
--- app.rb
+++ app.rb
@@ -7,12 +7,9 @@ require 'sass'
 require 'coffee-script'
 require 'sinatra/r18n'
 
-set :default_locale, 'fr'
-
-
 helpers do
   def partial(page, options={})
-    haml page, options.merge!(:layout => false)
+    haml(page, options.merge!(layout: false))
   end
 end
 
@@ -21,20 +18,19 @@ before do
 end
 
 get '/application.js' do
-  coffee :application, :views => 'assets'
+  coffee :application, views: 'assets'
 end
 
 get '/application.css' do
-  sass :application, :views => 'assets'
+  sass :application, views: 'assets'
 end
 
 get '/' do
   @works = {}
 
   @works['jardins'] = {
-    :name => 'Jardins au féminin',
-    :images =>
-    [
+    name: 'Jardins au féminin',
+    images: [
       ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
       ['5.jpg', '6.jpg', '7.jpg', '8.jpg'],
       ['9.jpg', '10.jpg', '11.jpg'],
@@ -43,26 +39,23 @@ get '/' do
   }
 
   @works['autoportraits'] = {
-    :name => "Autoportraits",
-    :images =>
-    [
+    name: "Autoportraits",
+    images: [
       ['1.jpg', '2.jpg', '3.jpg']
     ]
   }
 
   @works['eau'] = {
-    :name => "Le bord de l'eau",
-    :images =>
-    [
+    name: "Le bord de l'eau",
+    images: [
       ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
       ['5.jpg', '6.jpg', '7.jpg']
     ]
   }
 
   @works['dialogues'] = {
-    :name => "Dialogues",
-    :images =>
-    [
+    name: "Dialogues",
+    images: [
       ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
       ['5.jpg', '6.jpg', '7.jpg', '8.jpg'],
       ['9.jpg']
@@ -70,9 +63,8 @@ get '/' do
   }
 
   @works['geographies'] = {
-    :name => "Géo-graphies",
-    :images =>
-    [
+    name: "Géo-graphies",
+    images: [
       ['1.jpg', '3.jpg', '2.jpg', '4.jpg'],
       ['5.jpg', '7.jpg', '6.jpg'],
       ['8.jpg', '9.jpg', '10.jpg']
