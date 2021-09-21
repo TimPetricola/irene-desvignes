f1e8b81cc15b413540ada52dceea730bbce1a4c3
Tim Petricola <tim.petricola@gmail.com>
2021-09-21 09:49:27 +0200

    Remove phone number

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,10 +6,6 @@ const Contact = () => {
           <td className="col1">Email</td>
           <td>irene.desvignes9@gmail.com</td>
         </tr>
-        <tr>
-          <td className="col1">Téléphone</td>
-          <td>+33 6 12 91 42 61</td>
-        </tr>
       </table>
     </section>
   );
