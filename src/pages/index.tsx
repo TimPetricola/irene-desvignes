fd99431c20be16be92a48d82c95c629f7c1c29bb
Tim Petricola <tim.petricola@gmail.com>
2022-03-26 21:21:22 +0100

    Remove lead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 const Home = () => {
   return (
     <section id="home">
-      <p className="lead">
-        Expo&nbsp;&nbsp;&nbsp;&nbsp;FLUX ROSES - Vernissage 11 Septembre à 15h.
-        Galerie B+, Lyon.
-      </p>
       <div className="center">
         <img src="/images/home.jpg" />
       </div>
